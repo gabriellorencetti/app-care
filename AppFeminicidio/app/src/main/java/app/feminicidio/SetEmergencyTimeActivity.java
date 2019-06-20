f217b8b2f4723f19fae79b995c6e71d014249274
jpsecundino <jpsecundino@usp.br>
2019-06-20 17:18:13 -0300

    correção de bugs minimos

--- AppFeminicidio/app/src/main/java/app/feminicidio/SetEmergencyTimeActivity.java
+++ AppFeminicidio/app/src/main/java/app/feminicidio/SetEmergencyTimeActivity.java
@@ -4,11 +4,13 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.text.method.ScrollingMovementMethod;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
@@ -26,6 +28,10 @@ public class SetEmergencyTimeActivity extends AppCompatActivity {
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.set_emergency_time_popup);
+
+        // Inicializa sidebar menu;
+        NavigationView nvDrawer = (NavigationView) findViewById(R.id.sidebar);
+        setupDrawerContent(nvDrawer);
 
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
@@ -72,5 +78,60 @@ public class SetEmergencyTimeActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                selectItemDrawer(menuItem);
+                return true;
+            }
+        });
+    }
+
+    public void selectItemDrawer(MenuItem menuItem) {
+
+        switch (menuItem.getItemId()) {
+            case R.id.nav_main:
+                openMainActivity();
+                break;
+
+            case R.id.nav_denuncia:
+                openDenunciaActivity();
+                break;
+
+            case R.id.nav_telefones:
+                openTelefonesActivity();
+                break;
+
+            case R.id.nav_info:
+                openInfoActivity();
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void openMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void openTelefonesActivity() {
+        Intent intent = new Intent(this, TelefonesActivity.class);
+        startActivity(intent);
+    }
+
+    public void openDenunciaActivity() {
+        Intent intent = new Intent(this, DenunciaActivity.class);
+        startActivity(intent);
+    }
+
+    public void openInfoActivity() {
+        Intent intent = new Intent(this, InformacoesActivity.class);
+        startActivity(intent);
+    }
+
+
 }
 
